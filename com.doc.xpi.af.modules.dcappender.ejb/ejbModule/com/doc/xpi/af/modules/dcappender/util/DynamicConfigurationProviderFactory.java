2f8ab699bece91fcbdb62beba3fec8ef08dad6f7
Philippe <philippe.addor@gmail.com>
2021-02-26 18:05:46 +0100

    Update DynamicConfigurationProviderFactory.java
    Added the oAuth provide and removed the others.

--- com.doc.xpi.af.modules.dcappender.ejb/ejbModule/com/doc/xpi/af/modules/dcappender/util/DynamicConfigurationProviderFactory.java
+++ com.doc.xpi.af.modules.dcappender.ejb/ejbModule/com/doc/xpi/af/modules/dcappender/util/DynamicConfigurationProviderFactory.java
@@ -5,19 +5,16 @@ import java.lang.reflect.InvocationTargetException;
 public class DynamicConfigurationProviderFactory {
 
 	private static final String PROVIDER_CLASS_SUBPACKAGE = "provider";
-	private static final String PROVIDER_DEFAULT_CLASS_NAME = "DynamicConfigurationProviderDefault";
+	private static final String PROVIDER_DEFAULT_CLASS_NAME = "DynamicConfigurationProviderOAuthTokenLookup";
 
-	public DynamicConfigurationProvider getDynamicConfigurationProvider(
-			String dcProviderType, String dcProviderClassName)
-			throws DynamicConfigurationProviderFactoryException {
+	public DynamicConfigurationProvider getDynamicConfigurationProvider(String dcProviderType,
+			String dcProviderClassName) throws DynamicConfigurationProviderFactoryException {
 
-		if (dcProviderType
-				.equals(DynamicConfigurationProviderType.DC_PROVIDER_DEFAULT)) {
+		if (dcProviderType.equals(DynamicConfigurationProviderType.DC_PROVIDER_DEFAULT)) {
 			dcProviderClassName = PROVIDER_DEFAULT_CLASS_NAME;
 		}
 
-		DynamicConfigurationProvider dcProvider = this
-				.getDynamicConfigurationProviderClass(dcProviderClassName);
+		DynamicConfigurationProvider dcProvider = this.getDynamicConfigurationProviderClass(dcProviderClassName);
 
 		if (dcProvider != null) {
 			return dcProvider;
@@ -28,49 +25,40 @@ public class DynamicConfigurationProviderFactory {
 
 	}
 
-	private DynamicConfigurationProvider getDynamicConfigurationProviderClass(
-			String className)
+	private DynamicConfigurationProvider getDynamicConfigurationProviderClass(String className)
 			throws DynamicConfigurationProviderFactoryException {
 
 		String factoryPackageName = this.getClass().getPackage().getName();
-		String classPackageName = factoryPackageName.substring(0,
-				factoryPackageName.lastIndexOf('.'))
-				+ "." + PROVIDER_CLASS_SUBPACKAGE;
+		String classPackageName = factoryPackageName.substring(0, factoryPackageName.lastIndexOf('.')) + "."
+				+ PROVIDER_CLASS_SUBPACKAGE;
 		String classCanonicalName = classPackageName + "." + className;
 
 		try {
 			Class<?> classDcProvider = Class.forName(classCanonicalName);
-			DynamicConfigurationProvider dcProvider = (DynamicConfigurationProvider) classDcProvider
-					.getConstructor().newInstance();
+			DynamicConfigurationProvider dcProvider = (DynamicConfigurationProvider) classDcProvider.getConstructor()
+					.newInstance();
 			return dcProvider;
 		} catch (ClassNotFoundException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (IllegalArgumentException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (SecurityException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (InstantiationException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (IllegalAccessException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (InvocationTargetException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		} catch (NoSuchMethodException e) {
 			throw new DynamicConfigurationProviderFactoryException(
-					"Error when accessing dynamic configuration provider: "
-							+ e.getMessage(), e);
+					"Error when accessing dynamic configuration provider: " + e.getMessage(), e);
 		}
 
 	}
